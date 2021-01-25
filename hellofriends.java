5617f4c3047b4189002f74b9a43209716f81ad88
bujurke <ambikabujurke22@gmail.com>
2021-01-25 22:19:03 +0530

    second java program

---
+++ hellofriends.java
@@ -0,0 +1,12 @@
+public class hellofriends {
+
+       public static void main (string []args) {
+       //prints "hello, friends" to the terminal window.
+       system.out.println ("hello,friends");
+       
+    }
+    
+}       
+        
+       
+       
